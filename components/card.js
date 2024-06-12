78945e925cef7738399303dc42347b436718bcb9
Anderson Alves Santana <anderson.asantana05@gmail.com>
2024-06-12 20:59:43 -0300

    feat: atualizção de erros components

---
+++ components/card.js
@@ -0,0 +1,20 @@
+ export default {
+    name: 'Card',
+    props: {
+        personage:{
+            type: Object,
+            required: true
+        }
+    },
+    template: `
+    <div class="card">
+    <img :src="personage.image" width="50" height="70"> 
+        <p class="card-title">{{ personage.name }}</p>
+        <ul class="card-description">
+          <li>Força: {{ personage.ki }}</li>
+          <li>Maximo de Força: {{ personage.maxKi }}</li>
+          <li>raça: {{ personage.race }}</li>
+        </ul>
+      </div>
+      `
+ }
